5c0d5f7efddbfd988d6530b37e19c3e0e9d16b0e
SENC2023 <145979246+SENC2023@users.noreply.github.com>
2024-05-08 11:13:42 +0000

    Pendiente por terminar inicio de sesión 08/05/2024

---
+++ src/front/js/pages/inicioSesion.js
@@ -0,0 +1,13 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/home.css";
+
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+
+	return (
+		<div className="">
+			Hello, World
+		</div>
+	);
+};
