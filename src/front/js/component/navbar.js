bc2fcd79f98e52522a1ff8179fc69ff325b3d865
Jonayj0 <141333392+Jonayj0@users.noreply.github.com>
2024-05-08 13:01:53 +0000

    He hecho el navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,18 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/navbar.css";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
+		<nav className="navbar navbar-expand-lg bg-body-tertiary py-2 border-bottom">
+			<div className="container-fluid">
+				<a className="navbar-brand ms-5" href="/"><i className="logo fa-solid fa-street-view "></i></a>
+				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<ul className="nav justify-content-center">
+					<li className="nav-item border-end">
+						<a className="nav-link active" aria-current="page" href="#">Home</a>
+					</li>
+					<li className="nav-item border-end">
+						<a className="nav-link" href="/demo">Nosotros</a>
+					</li>
+					<li className="nav-item border-end">
+						<a className="nav-link" href="#">Tiendas</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">Productos</a>
+					</li>
+				</ul>
+					<button className="Login btn btn-success me-5 my-3" type="submit"><i className="fa-solid fa-circle-user fa-fade"></i>  Iniciar sesión</button>
 			</div>
 		</nav>
 	);
