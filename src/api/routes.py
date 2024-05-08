2e7ffa27fd1315f91591db84ba0bef416a1ff380
Jonayj0 <141333392+Jonayj0@users.noreply.github.com>
2024-05-08 14:15:09 +0000

    Haciendo Endpoint Login da error de Blueprint

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,12 +5,26 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
+# Handle/serialize errors like a JSON object
+@api.errorhandler(APIException)
+def handle_invalid_usage(error):
+    return jsonify(error.to_dict()), error.status_code
+
+# generate sitemap with all your endpoints--------------------------------------------------------------------------------
+@api.route('/')
+def sitemap():
+    return generate_sitemap(api)
+
+# Setup the Flask-JWT-Extended extension
+api.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+jwt = JWTManager(api)
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
@@ -20,3 +34,23 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+# LOGIN-------------------------------------------------------------------------------------------------------------------
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@api.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    check_vendedor = User.query.filter_by(email=email).first()
+    print(check_vendedor)
+
+    if check_vendedor is None:
+        return jsonify({"msg": "Email doesn't exist"}), 404
+
+    if email != check_vendedor.email or password != check_vendedor.password:
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
