5977e15a8ae1667f5fb9dd44ed5b6ec2cd3f7e9e
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2024-05-08 21:36:42 +0000

    Iniciado proyecto, añadida nueva página en front tienda_usuario

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -6,6 +6,7 @@ import { BackendURL } from "./component/backendURL";
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
+import { TiendaUsuario } from "./pages/tienda_usuario";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -27,6 +28,7 @@ const Layout = () => {
                     <Routes>
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
+                        <Route element={<TiendaUsuario />} path="/tienda_usuario" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
