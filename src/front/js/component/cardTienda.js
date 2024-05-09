1ec6646437aa74c9c24257d5c288ccc98976c366
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2024-05-09 21:37:24 +0000

    añadidos h4,p,h5 en el componente cardtienda y titulo tienda, modificado CSS

--- src/front/js/component/cardTienda.js
+++ src/front/js/component/cardTienda.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import "../../styles/tiendas.css";
 
-const CardTienda = ({ titulo, texto, boton }) => {
+const CardTienda = ({ titulo, texto, precio }) => {
   return (
-    <div className="card custom-card" style={{ width: '50%', margin: '0.1rem' }}>
-      <img src="https://via.placeholder.com/300" className="card-img-top" alt="..." />
-      <div className="card-body d-flex flex-column justify-content-between text-center">
-        <h6 className="card-title">{titulo}</h6>
+
+    <div className="card custom-card mt-5">
+      <img src="https://via.placeholder.com/300" className="card-img-top" alt="placeholder" />
+      <div className="card-body  flex-column">
+        <h4 className="card-title">{titulo}</h4>
         <p className="card-text">{texto}</p>
-        <div className="mt-auto btn-container">
-          <a href="#" className="btn btn-primary btn-sm d-block mx-auto" style={{ fontSize: '0.6rem', padding: '0.2rem 0.4rem', maxWidth: '6rem' }}>{boton}</a>
-        </div>
+        <h5 className="card-price">{precio}</h5>
       </div>
     </div>
+
+
   );
 };
 
--- src/front/js/component/tituloTienda.js
+++ src/front/js/component/tituloTienda.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
-const TituloTienda = ({ titulo, texto, boton }) => {
-  return (
-    <div className="text custom-titulo" style={{ width: '50%', margin: '0.1rem' }}>
-      
-      <div className="card-body d-flex flex-column justify-content-between text-center">
-        <h1 className="card-title">{titulo}</h1>
-        <p className="card-text">{texto}</p>
-       
-      </div>
-    </div>
-  );
+const TituloTienda = ({ titulo, descripcion }) => {
+    return (
+        <div className="text custom-titulo">
+
+            <div className="card-body d-flex flex-column justify-content-between text-center">
+                <h3 className="tienda-title mb-4">{titulo}</h3>
+                <p className="tienda-text mb-5">{descripcion}</p>
+
+
+            </div>
+        </div>
+    );
 };
 
 export default TituloTienda;
