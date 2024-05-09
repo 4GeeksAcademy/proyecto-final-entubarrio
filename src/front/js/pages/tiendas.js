489163e4700be1c7d8cb24dd96e475babf738939
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2024-05-09 16:50:23 +0000

    añadido componente card para la vista de la tienda

--- src/front/js/pages/tiendas.js
+++ src/front/js/pages/tiendas.js
@@ -2,25 +2,37 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import Card from "../component/cardTienda";
+
+
 
 export const Tiendas = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Tiendas</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+        <div className="cardcontainer">
+            <div className="row">
+                <div className="col-sm-12 col-md-3">
+                    <div className="card1">
+                        <Card titulo="Aleluya I" texto="Esta es la primera tarjeta y estoy muy contento" boton="Mágico"/>
+                    </div>
+                </div>
+                <div className="col-sm-12 col-md-3">
+                    <div className="card2">
+                        <Card titulo="Aleluya II" texto="Esta es la segunda tarjeta y estoy very happy" boton="Maravilloso" />
+                    </div>
+                </div>
+                <div className="col-sm-12 col-md-3">
+                    <div className="card3">
+                        <Card titulo="Aleluya III" texto="Esta es la tercera tarjeta y estoy muy cool " boton="Sorprendente"/>  
+                    </div>
+                </div>
+                <div className="col-sm-12 col-md-3">
+                    <div className="card4">
+                        <Card titulo="Aleluya IV" texto="Esta es la cuarta tarjeta y estoy molo mucho" boton="Viva la vida" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
