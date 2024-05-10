8eb946f5e8ec7209b65f0d8b63dc69534420851a
Jonayj0 <141333392+Jonayj0@users.noreply.github.com>
2024-05-10 21:09:17 +0000

    Titulo en Navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -10,13 +10,13 @@ export const Navbar = () => {
 
 
 	return (
-		<nav className="navbar navbar-expand-lg bg-body-tertiary py-2 border-bottom">
+		<nav className="navbar navbar-expand-lg bg-body-tertiary py-2 border-bottom d-flex flex-column">
 			<div className="container-fluid">
 				<a className="navbar-brand ms-5" href="/"><i className="logo fa-solid fa-street-view "></i></a>
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
 				</button>
-				<ul className="nav justify-content-center">
+				<ul className="nav justify-content-center ms-5">
 					<li className="nav-item border-end">
 						<a className="nav-link active" aria-current="page" href="/">Home</a>
 					</li>
@@ -34,6 +34,7 @@ export const Navbar = () => {
 					<button className="Login btn btn-success me-5 my-3" type="submit"><i className="fa-solid fa-circle-user fa-fade"></i>  Iniciar sesión</button>
 				</Link>
 			</div>
+			<a className="navbar-brand" href="/"><h1>En tu Barrio</h1></a>
 		</nav>
 	);
 };
