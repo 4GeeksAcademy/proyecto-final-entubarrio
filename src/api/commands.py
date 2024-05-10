7d94e89f25bf33114dd1c4c1aebf7189f7dfeb11
David Padilla Villén <149684862+Davpad@users.noreply.github.com>
2024-05-10 11:20:01 +0000

    Base con datos de ejemplo

--- src/api/commands.py
+++ src/api/commands.py
@@ -44,6 +44,126 @@ def setup_commands(app):
 
     #         print("All test users created")
 
+    @app.cli.command("fill-db-with-example-data")
+    def fill_db_with_example_data():
+        """ Este comando rellenará la base de datos con datos de ejemplo. """
+        db.drop_all()
+        db.create_all()
+        try:
+            vendedores = [
+                Vendedor(email="verduraspaco@gmail.com", password="111111"),
+                Vendedor(email="verdurasisa@gmail.com", password="123"),
+                Vendedor(email="delahuertaatucasa@gmail.com", password="puerros")
+            ]
+            db.session.add_all(vendedores)
+            db.session.commit()
+
+            tiendas = [
+                Tienda(nombre_tienda="Verduras Paco", categoria_tienda="Frutería", direccion_tienda="Calle cebolla", descripcion_tienda="La tienda de Paco te trae verduras a saco", url_imagen_tienda="https://frutasyverduraspaco.com/wp-content/themes/yootheme/cache/6306ce68-a34e-4cc9-ae8b-d8282936975a-a14cb339.jpg", vendedor_id=vendedores[0].id),
+                Tienda(nombre_tienda="Verduras Isa", categoria_tienda="Frutería", direccion_tienda="Calle canela", descripcion_tienda="En verduras Isa los precios dan risa", url_imagen_tienda="https://frutasmontijo.com/wp-content/uploads/2018/10/fruterias.jpg", vendedor_id=vendedores[1].id),
+                Tienda(nombre_tienda="La huerta en casa", categoria_tienda="Frutería", direccion_tienda="Calle Badajoz", descripcion_tienda="Nuestros productos vienen directos de la huerta", url_imagen_tienda="https://www.sanferescomercio.com/wp-content/uploads/2019/01/LAHUERTAENCASA6.jpg", vendedor_id=vendedores[2].id),
+            ]
+            db.session.add_all(tiendas)
+            db.session.commit()
+
+            productos = [
+                Producto(nombre_producto="Platanos", descripcion_producto="Directos de Canarias", precio="2", categoria_producto="Frutas", url_imagen_producto="https://sgfm.elcorteingles.es/SGFM/dctm/MEDIA03/202301/24/00118109600041____5__1200x1200.jpg", vendedor_id=vendedores[0].id, tienda_id=tiendas[0].id),
+                Producto(nombre_producto="Platanos", descripcion_producto="Directos de Canarias", precio="3", categoria_producto="Frutas", url_imagen_producto="https://sgfm.elcorteingles.es/SGFM/dctm/MEDIA03/202301/24/00118109600041____5__1200x1200.jpg", vendedor_id=vendedores[1].id, tienda_id=tiendas[1].id),
+                Producto(nombre_producto="Platanos", descripcion_producto="Directos de Canarias", precio="2", categoria_producto="Frutas", url_imagen_producto="https://sgfm.elcorteingles.es/SGFM/dctm/MEDIA03/202301/24/00118109600041____5__1200x1200.jpg", vendedor_id=vendedores[2].id, tienda_id=tiendas[2].id),
+                Producto(nombre_producto="Manzanas", descripcion_producto="Manzanas Gala rojas", precio="2", categoria_producto="Frutas", url_imagen_producto="https://tienda.ecohuertas.com.uy/imagenes/img_contenido/productos/a/gala.jpg", vendedor_id=vendedores[0].id, tienda_id=tiendas[0].id),
+                Producto(nombre_producto="Manzanas", descripcion_producto="Manzanas Gala rojas", precio="4", categoria_producto="Frutas", url_imagen_producto="https://tienda.ecohuertas.com.uy/imagenes/img_contenido/productos/a/gala.jpg", vendedor_id=vendedores[2].id, tienda_id=tiendas[2].id),
+                Producto(nombre_producto="Manzanas", descripcion_producto="Manzanas Gala rojas", precio="3", categoria_producto="Frutas", url_imagen_producto="https://tienda.ecohuertas.com.uy/imagenes/img_contenido/productos/a/gala.jpg", vendedor_id=vendedores[1].id, tienda_id=tiendas[1].id),
+                Producto(nombre_producto="Tomates", descripcion_producto="Tomates en rama", precio="3", categoria_producto="Verduras", url_imagen_producto="https://www.tastingtable.com/img/gallery/20-tricks-to-make-your-tomatoes-even-more-delicious/intro-1684770527.jpg", vendedor_id=vendedores[0].id, tienda_id=tiendas[0].id),
+                Producto(nombre_producto="Tomates", descripcion_producto="Tomates en rama", precio="4", categoria_producto="Verduras", url_imagen_producto="https://www.tastingtable.com/img/gallery/20-tricks-to-make-your-tomatoes-even-more-delicious/intro-1684770527.jpg", vendedor_id=vendedores[1].id, tienda_id=tiendas[1].id),
+                Producto(nombre_producto="Pimientos del padron", descripcion_producto="Unos pican y otros no", precio="5", categoria_producto="Verduras", url_imagen_producto="https://upload.wikimedia.org/wikipedia/commons/5/54/Pementos_de_Padron.jpg", vendedor_id=vendedores[1].id, tienda_id=tiendas[1].id),
+                Producto(nombre_producto="Pimientos del padron", descripcion_producto="Unos pican y otros no", precio="4", categoria_producto="Verduras", url_imagen_producto="https://upload.wikimedia.org/wikipedia/commons/5/54/Pementos_de_Padron.jpg", vendedor_id=vendedores[0].id, tienda_id=tiendas[0].id),
+                Producto(nombre_producto="Miel", descripcion_producto="Miel de Espliego", precio="9", categoria_producto="Miel", url_imagen_producto="https://upload.wikimedia.org/wikipedia/commons/5/54/Pementos_de_Padron.jpg", vendedor_id=vendedores[0].id, tienda_id=tiendas[0].id),
+                Producto(nombre_producto="Miel", descripcion_producto="Miel de Espliego", precio="10", categoria_producto="Miel", url_imagen_producto="https://upload.wikimedia.org/wikipedia/commons/5/54/Pementos_de_Padron.jpg", vendedor_id=vendedores[1].id, tienda_id=tiendas[1].id),
+                Producto(nombre_producto="Miel", descripcion_producto="Miel de Espliego", precio="11", categoria_producto="Miel", url_imagen_producto="https://upload.wikimedia.org/wikipedia/commons/5/54/Pementos_de_Padron.jpg", vendedor_id=vendedores[2].id, tienda_id=tiendas[2].id),
+            ]
+            db.session.add_all(productos)
+            db.session.commit()
+
+
+#             hobbies = [
+#                 Hobbie(name="Fotografía"),
+#                 Hobbie(name="Ciclismo"),
+#                 Hobbie(name="Dibujo"),
+#                 Hobbie(name="Programación")
+#             ]
+#             db.session.add_all(hobbies)
+#             db.session.commit()
+#             psychologists = [
+#                 Phycologyst(name="Ana", surnames="Pérez", email="
+# ana@mymood.com
+# ", password="secure", experience=5),
+#                 Phycologyst(name="Carlos", surnames="Gómez", email="
+# carlos@mymood.com
+# ", password="secure", experience=7)
+#             ]
+#             db.session.add_all(psychologists)
+#             db.session.commit()
+#             resource_types = [
+#                 ResourceType(resource_type="Artículo"),
+#                 ResourceType(resource_type="Vídeo"),
+#                 ResourceType(resource_type="Imagen"),
+#                 ResourceType(resource_type="Podcast")
+#             ]
+#             db.session.add_all(resource_types)
+#             db.session.commit()
+#             resources = [
+#                 Resource(resource_type_id=resource_types[0].id, url="
+# https://example.com/articulo1
+# ", description="Cómo manejar el estrés", phycologyst_id=psychologists[0].id),
+#                 Resource(resource_type_id=resource_types[1].id, url="https://example.com/video1", description="Meditación para principiantes", phycologyst_id=psychologists[1].id)
+#             ]
+#             db.session.add_all(resources)
+#             db.session.commit()
+#             locations = [
+#                 Location(latitude=40.7128, longitude=-74.0060),
+#                 Location(latitude=34.0522, longitude=-118.2437),
+#                 Location(latitude=41.8781, longitude=-87.6298)
+#             ]
+#             db.session.add_all(locations)
+#             db.session.commit()
+#             # Acciones relacionadas con las categorías de estados de ánimo
+#             actions = [
+#                 Action(action="Hablar con un amigo", description="Compartir tus sentimientos puede ayudar a ver las cosas desde otra perspectiva.", category_id=categories[0].id),  # Feliz/Contento
+#                 Action(action="Escribir en un diario", description="Escribir tus pensamientos puede ayudarte a comprenderlos mejor.", category_id=categories[1].id),  # Triste/Deprimido
+#                 Action(action="Meditación", description="La meditación puede ayudarte a calmar tu mente.", category_id=categories[2].id),  # Ansioso/Estresado
+#                 Action(action="Ejercicio físico", description="El ejercicio puede ayudar a liberar la tensión acumulada.", category_id=categories[3].id)  # Enojado/Frustrado
+#             ]
+#             db.session.add_all(actions)
+#             db.session.commit()
+#             # Historial de estados de ánimo de los usuarios
+#             user_mood_history_entries = [
+#                 UserMoodHistory(user_id=users[0].id, date=datetime.date.today() - datetime.timedelta(days=1), mood_id=moods[0].id),
+#                 UserMoodHistory(user_id=users[1].id, date=datetime.date.today() - datetime.timedelta(days=2), mood_id=moods[1].id),
+#                 UserMoodHistory(user_id=users[2].id, date=datetime.date.today() - datetime.timedelta(days=3), mood_id=moods[2].id)
+#             ]
+#             db.session.add_all(user_mood_history_entries)
+#             db.session.commit()
+#             # Chats entre usuarios
+#             chats = [
+#                 Chat(user_sender_id=users[0].id, user_reciver_id=users[1].id, message_text="¡Hola! ¿Cómo te sientes hoy?", time=datetime.datetime.now() - datetime.timedelta(hours=1)),
+#                 Chat(user_sender_id=users[1].id, user_reciver_id=users[0].id, message_text="Hola, me siento bastante bien, ¿y tú?", time=datetime.datetime.now() - datetime.timedelta(minutes=50)),
+#                 Chat(user_sender_id=users[0].id, user_reciver_id=users[1].id, message_text="También estoy bien, gracias por preguntar.", time=datetime.datetime.now() - datetime.timedelta(minutes=30)),
+#             ]
+#             db.session.add_all(chats)
+#             db.session.commit()
+            print("La base de datos ha sido poblada con datos de ejemplo.")
+        except Exception as e:
+            db.session.rollback()
+            print(f"Error al llenar la base de datos: {e}")
+
+
+
+
+
+
+
+
+
     @app.cli.command("insert-test-data")
     def insert_test_data():
         pass
