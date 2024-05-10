5cd1a6b628ad484752a42932fb16228c32e0f888
Adrian Torres <adriantorres.gc@gmail.com>
2024-05-10 09:48:19 +0000

    new file:   migrations/README 	new file:   migrations/alembic.ini 	new file:   migrations/env.py 	new file:   migrations/script.py.mako 	new file:   migrations/versions/94bd4efa54f1_.py 	new file:   src/front/img/barra_pan.jpg 	modified:   src/front/js/layout.js 	new file:   src/front/js/pages/info.js 	modified:   src/front/styles/index.css

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -10,6 +10,7 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import Informacion from "./pages/info";
 
 //create your first component
 const Layout = () => {
@@ -29,6 +30,7 @@ const Layout = () => {
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
+                        <Route element={<Informacion />} path="/info" />
                     </Routes>
                     <Footer />
                 </ScrollToTop>
