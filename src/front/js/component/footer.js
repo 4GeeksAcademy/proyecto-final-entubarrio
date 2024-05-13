f53916808125e1e11e6d4ed869078e3d4b02cc7b
SENC2023 <145979246+SENC2023@users.noreply.github.com>
2024-05-13 10:09:10 +0000

    Cambios en footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,24 +1,10 @@
 import React, { Component } from "react";
-import "../../styles/footer.css";
 
 export const Footer = () => (
-	<footer className="bottomSide bg-body-tertiary border-top py-3">
-		<ul className="nav justify-content-center">
-			<li className="nav-item border-end">
-				<a className="nav-link active" aria-current="page" href="/">Home</a>
-			</li>
-			<li className="nav-item border-end">
-				<a className="nav-link" href="/nosotros">Nosotros</a>
-			</li>
-			<li className="nav-item border-end">
-				<a className="nav-link" href="/tiendas">Tiendas</a>
-			</li>
-			<li className="nav-item">
-				<a className="nav-link" href="#">Productos</a>
-			</li>
-		</ul>
-		<div className="d-flex justify-content-center py-3">
-			<i class="fa-regular fa-copyright"> En tu barrio</i>
-		</div>
+	<footer className="footer mt-auto py-3 text-center">
+		<p>
+			Made with <i className="fa fa-heart text-danger" /> by{" "}
+			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+		</p>
 	</footer>
 );
