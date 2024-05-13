e69c52ecd8b088ff4bbc243a3ddb6efe69ceae71
Jonayj0 <141333392+Jonayj0@users.noreply.github.com>
2024-05-13 13:11:09 +0000

    Ediatdo el titulo y enlace

--- src/front/js/pages/nosotros.js
+++ src/front/js/pages/nosotros.js
@@ -34,15 +34,6 @@ export const Nosotros = () => {
             <a href="/login"><h4>¡Regístrate ahora!</h4></a>
 
             <p><h6>Juntos, podemos construir un futuro mejor para nuestros barrios.</h6></p>
-            {/* <p>
-				<img src={rigoImageUrl} />
-			</p> */}
-
-            {/* <p>
-                <a href="/login">
-                    Ahora que nos conoces ¿Te gustaría crear una cuenta?
-                </a>
-            </p> */}
         </div>
     );
 };
