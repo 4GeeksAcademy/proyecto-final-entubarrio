d95898357e5875d33c6318c365896989c8d98de5
Jonayj0 <141333392+Jonayj0@users.noreply.github.com>
2024-05-13 22:27:16 +0000

    Vista y ruta Productos

--- src/front/js/pages/productos.js
+++ src/front/js/pages/productos.js
@@ -9,6 +9,38 @@ export const Productos = () => {
     return (
         <>
         <h1>Productos</h1>
+        <h2>Seleccion de Productos</h2>
+			<p>Aqui podras encontrar una variedad de productos de tu barrio</p>
+			<div className="categorias-home container d-flex mb-5 justify-content-space-evenly" style={{ overflowBlock: "scroll" }}>
+				<div className="carrusel-home w-1/3 h-64 bg-zinc-800 flex-shrink-0">
+				<a href="/tiendas"><img src="https://placehold.co/200x200" alt="Image 1" className="w-full h-full object-cover" /></a>
+					<div className="absolute bottom-0 left-0 right-0 p-2">
+						<h3 className="text-black text-sm font-bold">Frutas</h3>
+						<p className="text-black text-xs">Frutas de temporada</p>
+					</div>
+				</div>
+				<div className="carrusel-home w-1/3 h-64 bg-zinc-800 flex-shrink-0 relative">
+				<a href="/tiendas"><img src="https://placehold.co/200x200" alt="Image 1" className="w-full h-full object-cover" /></a>
+					<div className="absolute bottom-0 left-0 right-0 p-2">
+						<h3 className="text-black text-sm font-bold">Verduras</h3>
+						<p className="text-black text-xs">Verduras y Hortalizas</p>
+					</div>
+				</div>
+				<div className="carrusel-home w-1/3 h-64 bg-zinc-800 flex-shrink-0 relative">
+					<a href="/tiendas"><img src="https://placehold.co/200x200" alt="Image 1" className="w-full h-full object-cover" /></a>
+					<div className="absolute bottom-0 left-0 right-0 p-2">
+						<h3 className="text-black text-sm font-bold">Pan</h3>
+						<p className="text-black text-xs">Pan casero</p>
+					</div>
+				</div>
+				<div className="carrusel-home w-1/3 h-64 bg-zinc-800 flex-shrink-0 relative">
+				<a href="/tiendas"><img src="https://placehold.co/200x200" alt="Image 1" className="w-full h-full object-cover" /></a>
+					<div className="absolute bottom-0 left-0 right-0 p-2">
+						<h3 className="text-black text-sm font-bold">Dulces</h3>
+						<p className="text-black text-xs">Dulces y tartas</p>
+					</div>
+				</div>
+			</div>
         {/* <div className="tarjeta-productos card mt-5">
             <img src="https://via.placeholder.com/200" className="card-img-top" alt="imagen ejemplo tarjeta" />
             <div className="card-body flex-column">
