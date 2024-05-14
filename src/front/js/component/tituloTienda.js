7a90c1a930dc29d83b4a8ac273679a387de43716
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2024-05-14 08:21:58 +0000

    hecha lógica en el componente titulo de la tienda para traer el flux y llamar a la acción, funciona con el get de ejemplo

--- src/front/js/component/tituloTienda.js
+++ src/front/js/component/tituloTienda.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { Context } from '../store/appContext'; // Importa el contexto
 
-const TituloTienda = ({ titulo, descripcion }) => {
+const TituloTienda = () => {
+    const { store, actions } = useContext(Context); // Obtiene el store y las acciones del contexto
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        actions.getMessage(); // Llama a la acción getMessage cuando el componente se monta
+    }, [actions]);
+
+    useEffect(() => {
+        // Establece loading a false cuando el mensaje se ha cargado en el store
+        if (store.message !== null) {
+            setLoading(false);
+        }
+    }, [store.message]);
+
     return (
         <div className="text custom-titulo">
-
-            <div className="card-body d-flex flex-column justify-content-between text-center">
-                <h3 className="tienda-title mb-4">{titulo}</h3>
-                <p className="tienda-text mb-5">{descripcion}</p>
-
-
-            </div>
+            {loading ? (
+                <p>Cargando...</p>
+            ) : (
+                <div className="card-body d-flex flex-column justify-content-between text-center">
+                    <h3 className="tienda-title mb-4">{store.message}</h3>
+                </div>
+            )}
         </div>
     );
 };
