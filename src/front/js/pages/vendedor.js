f84398f36c3e983a30817b7aa3591d61b5bd7728
Jonayj0 <141333392+Jonayj0@users.noreply.github.com>
2024-05-14 21:15:38 +0000

    añadida foto de tienda en vista vendedor

--- src/front/js/pages/vendedor.js
+++ src/front/js/pages/vendedor.js
@@ -7,49 +7,18 @@ export const Vendedor = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-0">
-			<p>
+		<div className="text-center mt-5">
+			<h2>Fruteria Juani</h2>
+			<p></p>
 				<img src={fruteria}
 					className="imagen-fruteria img-fluid w-150"
 					alt="Foto Home" />
-			</p>
+			
 			<div className="alert alert-info">
 				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
 			</div>
-			<h2>Seleccion de Tiendas</h2>
-			<p>Aqui podras encontrar una variedad de tiendas de tu barrio</p>
-			<div className="categorias-home container-fluid d-flex mb-5" style={{ overflowX: "scroll" }}>
-				<div className="carrusel-home w-1/3 h-64 bg-zinc-800 flex-shrink-0">
-				<a href="/tiendas"><img src="https://placehold.co/200x200" alt="Image 1" className="w-full h-full object-cover" /></a>
-					<div className="absolute bottom-0 left-0 right-0 p-2">
-						<h3 className="text-black text-sm font-bold">Fruteria Juani</h3>
-						<p className="text-black text-xs">Frutas de temporada</p>
-					</div>
-				</div>
-				<div className="carrusel-home w-1/3 h-64 bg-zinc-800 flex-shrink-0 relative">
-				<a href="/tiendas"><img src="https://placehold.co/200x200" alt="Image 1" className="w-full h-full object-cover" /></a>
-					<div className="absolute bottom-0 left-0 right-0 p-2">
-						<h3 className="text-black text-sm font-bold">Verduras Paco</h3>
-						<p className="text-black text-xs">Verduras y Hortalizas</p>
-					</div>
-				</div>
-				<div className="carrusel-home w-1/3 h-64 bg-zinc-800 flex-shrink-0 relative">
-					<a href="/tiendas"><img src="https://placehold.co/200x200" alt="Image 1" className="w-full h-full object-cover" /></a>
-					<div className="absolute bottom-0 left-0 right-0 p-2">
-						<h3 className="text-black text-sm font-bold">Pan para hoy</h3>
-						<p className="text-black text-xs">Pan casero</p>
-					</div>
-				</div>
-				<div className="carrusel-home w-1/3 h-64 bg-zinc-800 flex-shrink-0 relative">
-				<a href="/tiendas"><img src="https://placehold.co/200x200" alt="Image 1" className="w-full h-full object-cover" /></a>
-					<div className="absolute bottom-0 left-0 right-0 p-2">
-						<h3 className="text-black text-sm font-bold">Pasteles Goloso</h3>
-						<p className="text-black text-xs">Dulces y tartas</p>
-					</div>
-				</div>
-			</div>
-			<h2>Seleccion de Productos</h2>
-			<p>Aqui podras encontrar una variedad de productos de tu barrio</p>
+			<h2>Nuestros de Productos</h2>
+			<p>Aqui podras encontrar todos nuestros productos</p>
 			<div className="categorias-home container-fluid d-flex mb-5" style={{ overflowX: "scroll" }}>
 				<div className="carrusel-home w-1/3 h-64 bg-zinc-800 flex-shrink-0">
 				<a href="/tiendas"><img src="https://placehold.co/200x200" alt="Image 1" className="w-full h-full object-cover" /></a>
