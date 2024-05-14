07d149f1fb8655f6762144b82c8e394aba880d10
Jonayj0 <141333392+Jonayj0@users.noreply.github.com>
2024-05-14 12:04:24 +0000

    ajustes container-fluid home

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -18,7 +18,7 @@ export const Home = () => {
 			</div>
 			<h2>Seleccion de Tiendas</h2>
 			<p>Aqui podras encontrar una variedad de tiendas de tu barrio</p>
-			<div className="categorias-home container d-flex mb-5 justify-content-space-evenly" style={{ overflowX: "scroll" }}>
+			<div className="categorias-home container-fluid d-flex mb-5" style={{ overflowX: "scroll" }}>
 				<div className="carrusel-home w-1/3 h-64 bg-zinc-800 flex-shrink-0">
 				<a href="/tiendas"><img src="https://placehold.co/200x200" alt="Image 1" className="w-full h-full object-cover" /></a>
 					<div className="absolute bottom-0 left-0 right-0 p-2">
@@ -50,7 +50,7 @@ export const Home = () => {
 			</div>
 			<h2>Seleccion de Productos</h2>
 			<p>Aqui podras encontrar una variedad de productos de tu barrio</p>
-			<div className="categorias-home container d-flex mb-5 justify-content-space-evenly" style={{ overflowX: "scroll" }}>
+			<div className="categorias-home container-fluid d-flex mb-5" style={{ overflowX: "scroll" }}>
 				<div className="carrusel-home w-1/3 h-64 bg-zinc-800 flex-shrink-0">
 				<a href="/tiendas"><img src="https://placehold.co/200x200" alt="Image 1" className="w-full h-full object-cover" /></a>
 					<div className="absolute bottom-0 left-0 right-0 p-2">
