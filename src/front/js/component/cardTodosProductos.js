84f84739fd5158a96455500c6cfde79470eb2e52
Jonayj0 <141333392+Jonayj0@users.noreply.github.com>
2024-05-15 13:49:24 +0000

    intento añadir a todos productos nombre tienda

--- src/front/js/component/cardTodosProductos.js
+++ src/front/js/component/cardTodosProductos.js
@@ -5,15 +5,17 @@ import "../../styles/todosproductos.css";
 export const TodosProductos = ({url_imagen_producto,nombre_producto,descripcion_producto,id,precio}) => {
     const { store, actions } = useContext(Context);
 
+    // const nombre_tienda = store?.tiendas?.nombre_tienda
     return (
         <div>
-            <div className="grid-container d-flex mb-5 justify-content-space-evenly" style={{ overflowBlock: "scroll" }} key= {id}>
+            <div className="grid-container d-flex mb-2 mt-2 justify-content-space-evenly" style={{ overflowBlock: "scroll" }} key= {id}>
                 <div className="carrusel-home w-1/3 h-64 bg-zinc-800 flex-shrink-0">
                     <a href="/infoproducto"><img src={url_imagen_producto} alt="Image 1" className="img-todos-productos w-20 h-20 object-fit" /></a>
                     <div className="absolute bottom-0 left-0 right-0 p-2">
                         <h4 className="text-black text-sm font-bold">{nombre_producto}</h4>
                         <p className="text-black text-xs">{descripcion_producto}</p>
                         <h5 className="text-black">{precio}€</h5>
+                        {/* <h5 className="text-black">{store.tiendas.nombre_tienda}</h5> */}
                     </div>
                 </div>
             </div>
