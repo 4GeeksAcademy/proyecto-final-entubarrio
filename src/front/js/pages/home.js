eae901cdcca267e24de0cd303e77961466ec659d
David Padilla Villén <149684862+Davpad@users.noreply.github.com>
2024-05-15 09:54:11 +0000

    Arreglados los errores

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -32,7 +32,7 @@ export const Home = () => {
 			<div className="categorias-home container-fluid d-flex mb-5" style={{ overflowX: "scroll" }}>
 				{store.tiendas.map((tienda) =>{
 					return (
-						<TodasTiendas nombre_tienda = {tienda.nombre_tienda} id ={tienda.id} url_imagen_tienda={tienda.url_imagen_tienda} descripcion_tienda={tienda.descripcion_tienda} />
+						<TodasTiendas nombre_tienda = {tienda.nombre_tienda} key={tienda.id} id ={tienda.id} url_imagen_tienda={tienda.url_imagen_tienda} descripcion_tienda={tienda.descripcion_tienda} />
 					)
 				})}
 			</div>
@@ -41,7 +41,7 @@ export const Home = () => {
 			<div className="categorias-home container-fluid d-flex mb-5" style={{ overflowX: "scroll" }}>
 			{store.productos.map((producto) =>{
 					return (
-						<TodosProductos nombre_producto = {producto.nombre_producto} id ={producto.id} url_imagen_producto={producto.url_imagen_producto} descripcion_producto={producto.descripcion_producto} precio={producto.precio}/>
+						<TodosProductos nombre_producto = {producto.nombre_producto} key={producto.id} id ={producto.id} url_imagen_producto={producto.url_imagen_producto} descripcion_producto={producto.descripcion_producto} precio={producto.precio}/>
 					)
 				})}
 			</div>
