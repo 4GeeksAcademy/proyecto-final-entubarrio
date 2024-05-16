83e14498bf000151f5d01dca7b3e53ab376c9d8d
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2024-05-16 21:58:26 +0000

    justado hover para boton y pequeña animacion

--- src/front/js/component/cardVendedorTodosProductos.js
+++ src/front/js/component/cardVendedorTodosProductos.js
@@ -1,25 +1,29 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/todosproductos.css";
 
-export const TodosProductosVendedor = ({url_imagen_producto,nombre_producto,descripcion_producto,id,precio}) => {
-    const { store, actions } = useContext(Context);
+export const TodosProductosVendedor = ({url_imagen_producto, nombre_producto, descripcion_producto, id, precio}) => {
+    const { store } = useContext(Context);
 
-    const nombre_tienda = store?.tiendas?.nombre_tienda
     return (
         <div>
-            <div className="grid-container d-flex mb-2 mt-2 justify-content-space-evenly" style={{ overflowBlock: "scroll" }} key= {id}>
-                <h6>Esto es un componente</h6>
+            <div className="grid-container d-flex mb-2 mt-2 justify-content-center" style={{ overflowBlock: "scroll" }} key={id}>
+                <p>ESTO ES UN COMPONENTE</p>
                 <p>Aquí se renderizarán en breve (o eso espero) los productos de la tienda específica</p>
+                <a href="/infoproducto"><img src={"https://cdn-icons-png.freepik.com/256/12309/12309534.png?uid=R149155504&ga=GA1.1.2047492528.1715896395&semt=ais_hybrid"} alt="Imagen del producto" className="img-todos-productos w-20 h-20 object-fit" /></a>
                 <div className="carrusel-home w-1/3 h-64 bg-zinc-800 flex-shrink-0">
-                    <a href="/infoproducto"><img src={url_imagen_producto} alt="Image 1" className="img-todos-productos w-20 h-20 object-fit" /></a>
+                    
                     <div className="absolute bottom-0 left-0 right-0 p-2">
                         <h4 className="text-black text-sm font-bold">{nombre_producto}</h4>
                         <p className="text-black text-xs">{descripcion_producto}</p>
-                        <h5 className="text-black">{precio}€</h5>
-                        <h5 className="text-black">{store.tiendas.nombre_tienda}</h5> 
+                        <h5 className="text-black">{precio}</h5>
+                        <h5 className="text-black">{store?.tiendas?.nombre_tienda}</h5> 
                     </div>
                 </div>
+            </div>
+            <div className="en-obras">
+                <i className="fas fa-tools icono-en-obras"></i>
+                <h3 className="text-center">En obras</h3>
             </div>
         </div>
     );
