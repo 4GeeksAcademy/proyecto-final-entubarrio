cede3038241a3e8eabd1db73c70471fd6e2e1842
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2024-05-17 10:52:04 +0000

    metiendo funconalidad a la vista vendedor

--- src/front/js/component/cardVendedorTodosProductos.js
+++ src/front/js/component/cardVendedorTodosProductos.js
@@ -12,28 +12,27 @@ export const TodosProductosVendedor = ({ id }) => {
 
     return (
         <div>
-            {store.productosTienda ? (
-                store.productosTienda.map(producto => (
-                    <div className="grid-container d-flex mb-2 mt-2 justify-content-center" style={{ overflowBlock: "scroll" }} key={producto.id}>
-                        <p>ESTO ES UN COMPONENTE</p>
-                        <p>Aquí se renderizarán en breve (o eso espero) los productos de la tienda específica</p>
-                        <a href="/infoproducto"><img src={"https://cdn-icons-png.freepik.com/256/12309/12309534.png?uid=R149155504&ga=GA1.1.2047492528.1715896395&semt=ais_hybrid"} alt="Imagen del producto" className="img-todos-productos w-20 h-20 object-fit" /></a>
-                        <div className="carrusel-home w-1/3 h-64 bg-zinc-800 flex-shrink-0">
-                            <div className="absolute bottom-0 left-0 right-0 p-2">
-                                <h4 className="text-black text-sm font-bold">{producto.nombre_producto}</h4>
-                                <p className="text-black text-xs">{producto.descripcion_producto}</p>
-                                <p className="text-black text-xs">{producto.url_imagen_producto}</p>
-                                <h5 className="text-black">{producto.precio}</h5>
-                            </div>
-                        </div>
+            <h2>Seleccion de Productos</h2>
+            <p>Aquí podrás encontrar una variedad de productos de tu barrio</p>
+            <div className="categorias-home container-fluid d-flex mb-5" style={{ overflowX: "scroll" }}>
+                {store.productosTienda ? (
+                    store.productosTienda.map(producto => (
+                        <TodosProductos
+                            key={producto.id}
+                            nombre_producto={producto.nombre_producto}
+                            id={producto.id}
+                            url_imagen_producto={producto.url_imagen_producto}
+                            descripcion_producto={producto.descripcion_producto}
+                            precio={producto.precio}
+                        />
+                    ))
+                ) : (
+                    <div className="en-obras">
+                        <i className="fas fa-tools icono-en-obras"></i>
+                        <h3 className="text-center">En obras</h3>
                     </div>
-                ))
-            ) : (
-                <div className="en-obras">
-                    <i className="fas fa-tools icono-en-obras"></i>
-                    <h3 className="text-center">En obras</h3>
-                </div>
-            )}
+                )}
+            </div>
         </div>
     );
 };
