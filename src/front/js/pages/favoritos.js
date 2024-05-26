ec991f1e6892f721a19277a920f9756226c7a9bb
Jonayj0 <141333392+Jonayj0@users.noreply.github.com>
2024-05-26 01:27:32 +0000

    añadida vista mis favoritos

---
+++ src/front/js/pages/favoritos.js
@@ -0,0 +1,16 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+
+
+export const Favoritos = () => {
+
+    return (
+        <div className="text-center mt-5 mb-5">
+            <h1>Mis Tiendas Favoritas</h1><br></br>
+
+            <div className="text-center mt-5 mb-5">
+            <h1>Mis Productos Favoritos</h1><br></br>
+                </div>
+        </div>
+    );
+};
