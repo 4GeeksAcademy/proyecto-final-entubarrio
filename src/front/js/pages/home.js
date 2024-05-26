177ffe6a127c830352748ec9bfdcbd969f3f7794
SENC2023 <145979246+SENC2023@users.noreply.github.com>
2024-05-26 19:17:52 +0000

    Carrusel de tiendas y productos terminado, junto con los iconos

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,43 +4,104 @@ import imagenbarrio from "../../img/Barrio-Gracia-Barcelona_1394570563_109101042
 import "../../styles/home.css";
 import { TodosProductos } from "../component/cardTodosProductos";
 import { TodasTiendas } from "../component/cardTodasTiendas";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+    const { store, actions } = useContext(Context);
 
-	useEffect(() => {
-		actions.getTiendas()
-		actions.getProductos()
-		actions.getCategoriasProductos()
+    useEffect(() => {
+        actions.getTiendas();
+        actions.getProductos();
+        actions.getCategoriasProductos();
+    }, []);
 
-	}, []);
-	console.log(store.categoriasProductos);
+    const responsive = {
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 4,
+            slidesToSlide: 1
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2,
+            slidesToSlide: 1
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+            slidesToSlide: 1
+        }
+    };
 
-	return (
-		<div className="home text-center mt-0">
-			<p>
-				<img src={imagenbarrio}
-					className="imagen-barrio img-fluid w-150"
-					alt="Foto Home" />
-			</p>
-			<h2>Seleccion de Tiendas</h2>
-			<p>Aqui podras encontrar una variedad de tiendas de tu barrio</p>
-			<div className="categorias-home container-fluid d-flex mb-5" style={{ overflowX: "scroll" }}>
-				{store.tiendas.map((tienda) => {
-					return (
-						<TodasTiendas nombre_tienda={tienda.nombre_tienda} key={tienda.id} id={tienda.id} url_imagen_tienda={tienda.url_imagen_tienda} descripcion_tienda={tienda.descripcion_tienda} direccion_tienda={tienda.direccion_tienda} />
-					)
-				})}
-			</div>
-			<h2>Seleccion de Productos</h2>
-			<p>Aqui podras encontrar una variedad de productos de tu barrio</p>
-			<div className="categorias-home container-fluid d-flex mb-5" style={{ overflowX: "scroll" }}>
-				{store.productos.map((producto) => {
-					return (
-						<TodosProductos nombre_producto={producto.nombre_producto} key={producto.id} id={producto.id} url_imagen_producto={producto.url_imagen_producto} descripcion_producto={producto.descripcion_producto} precio={producto.precio} tienda_id={producto.tienda_id} nombre_tienda={producto.nombre_tienda} />
-					)
-				})}
-			</div>
-		</div>
-	);
+    const CustomLeftArrow = ({ onClick }) => {
+        return (
+            <button onClick={onClick} className="btn btn-circle btn-dark opacity-75 custom-arrow left-arrow">
+                <FaArrowLeft />
+            </button>
+        );
+    };
+
+    const CustomRightArrow = ({ onClick }) => {
+        return (
+            <button onClick={onClick} className="btn btn-circle btn-dark opacity-75 custom-arrow right-arrow">
+                <FaArrowRight />
+            </button>
+        );
+    };
+
+    return (
+        <div className="home text-center mt-0">
+            <p>
+                <img 
+                    src={imagenbarrio}
+                    className="imagen-barrio img-fluid w-150"
+                    alt="Foto Home" 
+                />
+            </p>
+            <h2>Selección de Tiendas</h2>
+            <p>Aquí podrás encontrar una variedad de tiendas de tu barrio</p>
+            <div className="categorias-home container-fluid mb-5">
+                <Carousel 
+                    responsive={responsive} 
+                    customLeftArrow={<CustomLeftArrow />} 
+                    customRightArrow={<CustomRightArrow />}
+                >
+                    {store.tiendas.map((tienda) => (
+                        <TodasTiendas 
+                            nombre_tienda={tienda.nombre_tienda} 
+                            key={tienda.id} 
+                            id={tienda.id} 
+                            url_imagen_tienda={tienda.url_imagen_tienda} 
+                            descripcion_tienda={tienda.descripcion_tienda} 
+                            direccion_tienda={tienda.direccion_tienda} 
+                        />
+                    ))}
+                </Carousel>
+            </div>
+            <h2>Selección de Productos</h2>
+            <p>Aquí podrás encontrar una variedad de productos de tu barrio</p>
+            <div className="categorias-home container-fluid mb-5">
+                <Carousel 
+                    responsive={responsive} 
+                    customLeftArrow={<CustomLeftArrow />} 
+                    customRightArrow={<CustomRightArrow />}
+                >
+                    {store.productos.map((producto) => (
+                        <TodosProductos 
+                            nombre_producto={producto.nombre_producto} 
+                            key={producto.id} 
+                            id={producto.id} 
+                            url_imagen_producto={producto.url_imagen_producto} 
+                            descripcion_producto={producto.descripcion_producto} 
+                            precio={producto.precio} 
+                            tienda_id={producto.tienda_id} 
+                            nombre_tienda={producto.nombre_tienda} 
+                        />
+                    ))}
+                </Carousel>
+            </div>
+        </div>
+    );
 };
