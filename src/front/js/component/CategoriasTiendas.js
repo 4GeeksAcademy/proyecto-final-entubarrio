34353a021c5e1c8f65059f2f108046c46d96e987
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2024-05-26 04:03:56 +0000

    añadido categorias tienda componente

---
+++ src/front/js/component/CategoriasTiendas.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { Context } from '../store/appContext'; 
+import "../../styles/tienda.css";
+
+const CategoriasTiendas = ({ onCategoriaChange }) => {
+    const { store, actions } = useContext(Context);
+    const [opcionSeleccionada, setOpcionSeleccionada] = useState('');
+
+    useEffect(() => {
+        actions.getCategoriasTiendas(); 
+    }, []);
+
+    const handleChange = (event) => {
+        const opcion = event.target.value;
+        setOpcionSeleccionada(opcion);
+        onCategoriaChange(opcion); 
+    };
+
+    return (
+        <div className="custom-categoria-tienda">
+            <div className="custom-header">
+                <h5 className="custom-titulo">Selecciona Categoria</h5>
+                <select
+                    className="form-select-tienda"
+                    aria-label="Selecciona Tiendas"
+                    value={opcionSeleccionada}
+                    onChange={handleChange}
+                >
+                    <option value="">Selecciona</option>
+                    {store.categoriasTiendas.map((categoria, index) => (
+                        <option key={index} value={categoria}>
+                            {categoria}
+                        </option>
+                    ))}
+                </select>
+            </div>
+            <div className="cardcontainertiendas">
+                {opcionSeleccionada && (
+                    <h3>{opcionSeleccionada}</h3>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default CategoriasTiendas;
