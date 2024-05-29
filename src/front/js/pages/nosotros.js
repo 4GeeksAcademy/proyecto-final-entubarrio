87d033138408a541781a88c915e7b43b4cd224a2
Adrian Torres <adriantorres.gc@gmail.com>
2024-05-29 11:58:39 +0000

    modified:   src/front/js/pages/nosotros.js 	modified:   src/front/styles/nosotros.css

--- src/front/js/pages/nosotros.js
+++ src/front/js/pages/nosotros.js
@@ -9,33 +9,68 @@ import "../../img/02woman.jpg"
 export const Nosotros = () => {
 
     return (
-        <div className="text-center pt-5 pb-5 fondo-amarillo2 container">
+        <div className="text-center pt-5 pb-5  container ">
 
             <h1>Bienvenido</h1><br></br>
 
-            <h3 className="pb-3">¡En tu barrio te espera un mundo de posibilidades!</h3>
+            <h3 className="text-danger-emphasis pb-3">¡En tu barrio te espera un mundo de posibilidades!</h3>
 
 
-            <div className="container text-center fondo-amarillo pt-3 pb-3 ">
-                <div className="row row-cols-3 ">
-                    <div className="col img-fluid "><img src="01woman.jpg"  ></img></div>
-                    <div className="col text-start m-2 ">
-                        <ul type="none">
-                            <li className="text-danger-emphasis "><strong>¿Cansado de las grandes superficies y de la impersonalidad de las compras online?</strong></li>
-                            <li>
-                                En tu barrio queremos acercarte a los comercios de tu barrio, esos pequeños negocios llenos de encanto y
-                                productos únicos que hacen de nuestra comunidad un lugar especial.</li></ul></div>
 
-                </div>
+
+            <div className=" text-center  pt-3 pb-3  ">
+
+
             </div>
+
+
+
+            <div className="clearfix">
+                <img src="01woman.jpg" className=" img-nosotros col-md-6 float-md-start mb-3 ms-md-4 " alt="..." />
+
+                <p className="text-danger-emphasis p-3">
+                    <strong>¿Cansado de las grandes superficies y de la impersonalidad de las compras online?</strong>
+                </p>
+
+                <p className="m-3">
+                    En tu barrio queremos acercarte a los comercios de tu barrio, esos pequeños negocios llenos de encanto y
+                    productos únicos que hacen de nuestra comunidad un lugar especial.
+                </p>
+
+            </div>
+
+
+
+
+            <h2 className="text-danger-emphasis p-3">¿Qué te ofrece En tu barrio?</h2>
+
             <br></br>
 
+            <div className="clearfix text-start">
+                <img src="02woman.jpg" className=" img-nosotros2 col-md-6 float-md-end mb-3 ms-md-4 " alt="..." />
 
+                <p className="text-danger-emphasis p-3">
+                <strong>Un centro comercial virtual para tu barrio: </strong>
+                        Descubre una amplia variedad de productos y servicios a tu alcance,
+                        desde tiendas de alimentación hasta librerías,
+                        pasando por talleres de artesanía y mucho más. Todo ello, sin moverte de casa y con la comodidad de comprar online.
+                </p>
 
+                <p className="text-danger-emphasis m-3">
+                <strong>Apoya al comercio local: </strong>Cada compra que realizas En tu barrio contribuye a fortalecer la economía de tu barrio y a crear un futuro más sostenible.
+                </p>
+                <p className="text-danger-emphasis m-3">
+                <strong>Productos frescos y de calidad: </strong>Disfruta de productos frescos y de temporada de la mano de comercios locales que apuestan por la calidad y la atención al cliente.
+                </p>
+               
+                <p className="text-danger-emphasis m-3">
+                <strong>Precios competitivos: </strong>Compara precios y encuentra las mejores ofertas en productos de tus tiendas favoritas.
+                </p>
 
-            <h2>¿Qué te ofrece En tu barrio?</h2>
+            </div>
+
             <br></br>
-            <div className="lista-usuarios container text-start px-4 fondo-amarillo ">
+            {/* <div className=" container text-start px-4  ">
                 <ul className=" Lista-propuestas row row-cols-2 " type="none" >
                     <div ><li className="text-danger-emphasis m-2"> <strong>Un centro comercial virtual para tu barrio: </strong>
                         Descubre una amplia variedad de productos y servicios a tu alcance,
@@ -46,7 +81,7 @@ export const Nosotros = () => {
                         <li className="text-danger-emphasis"><strong>Comodidad y rapidez: </strong>Recibe tus compras a domicilio o recógelas en la tienda, ¡tú eliges! Además, te ofrecemos un sistema de pago seguro y fácil de usar.</li>
                         <li className="text-danger-emphasis"><strong>Precios competitivos: </strong>Compara precios y encuentra las mejores ofertas en productos de tus tiendas favoritas.</li>
                     </div><div><img src="02woman.jpg" className="img-fluid my-5" ></img></div></ul>
-            </div>
+            </div> */}
 
             <br></br>
             <h2>¿Eres un pequeño comerciante?</h2>
