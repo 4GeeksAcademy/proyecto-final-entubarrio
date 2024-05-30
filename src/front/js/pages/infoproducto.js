ac1224f47fa80f8957b729eb08880dd91a05aed3
SENC2023 <145979246+SENC2023@users.noreply.github.com>
2024-05-30 10:12:07 +0000

    Carrusel en la vista de infoproducto

--- src/front/js/pages/infoproducto.js
+++ src/front/js/pages/infoproducto.js
@@ -1,36 +1,92 @@
 import React, { useContext, useEffect } from "react";
-import { Link } from "react-router-dom";
 import Info from "../component/info";
-import CardProductos from "../component/cardProductos";
 import { useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 import { TodosProductos } from "../component/cardTodosProductos";
-
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 
 export const Infoproducto = () => {
-
   const { store, actions } = useContext(Context);
+  const params = useParams();
 
-  const params = useParams()
   useEffect(() => {
-    actions.getProducto(params.id)
+    actions.getProducto(params.id);
     actions.getProductosTienda(params.tienda_id);
   }, []);
 
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4,
+      slidesToSlide: 1
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 1
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1
+    }
+  };
+
+  const CustomLeftArrow = ({ onClick }) => {
+    return (
+      <button onClick={onClick} className="btn btn-circle btn-dark opacity-75 custom-arrow left-arrow">
+        <FaArrowLeft />
+      </button>
+    );
+  };
+
+  const CustomRightArrow = ({ onClick }) => {
+    return (
+      <button onClick={onClick} className="btn btn-circle btn-dark opacity-75 custom-arrow right-arrow">
+        <FaArrowRight />
+      </button>
+    );
+  };
 
   return (
     <>
-      <div className="container pt-3 " style={{backgroundColor:"#def4f5"}}>
+      <div className="container pt-3 " style={{ backgroundColor: "#def4f5" }}>
         <div className="card-body">
-          <div><Info nombre_producto={store.producto.nombre_producto} key={store.producto.id} url_imagen_producto={store.producto?.url_imagen_producto} descripcion_producto={store.producto.descripcion_producto} precio={store.producto.precio} id={store.producto.id} nombre_tienda={store.producto.nombre_tienda} isFavorito={store.producto.isFavorito} /></div>
+          <div>
+            <Info 
+              nombre_producto={store.producto.nombre_producto} 
+              key={store.producto.id} 
+              url_imagen_producto={store.producto?.url_imagen_producto} 
+              descripcion_producto={store.producto.descripcion_producto} 
+              precio={store.producto.precio} 
+              id={store.producto.id} 
+              nombre_tienda={store.producto.nombre_tienda} 
+              isFavorito={store.producto.isFavorito} 
+            />
+          </div>
         </div>
       </div>
-      <div className="container categorias-home container-fluid d-flex mb-5" style={{ overflowX: "scroll" }}>
-        {store.productosTienda.map((producto) => {
-          return (
-            <TodosProductos nombre_producto={producto.nombre_producto} key={producto.id} id={producto.id} url_imagen_producto={producto.url_imagen_producto} descripcion_producto={producto.descripcion_producto} nombre_tienda={producto.nombre_tienda} precio={producto.precio} tienda_id={producto.tienda_id} />
-          )
-        })}
+      <div className="container categorias-home container-fluid mb-5">
+        <Carousel 
+          responsive={responsive} 
+          customLeftArrow={<CustomLeftArrow />} 
+          customRightArrow={<CustomRightArrow />}
+        >
+          {store.productosTienda.map((producto) => (
+            <TodosProductos 
+              nombre_producto={producto.nombre_producto} 
+              key={producto.id} 
+              id={producto.id} 
+              url_imagen_producto={producto.url_imagen_producto} 
+              descripcion_producto={producto.descripcion_producto} 
+              nombre_tienda={producto.nombre_tienda} 
+              precio={producto.precio} 
+              tienda_id={producto.tienda_id} 
+            />
+          ))}
+        </Carousel>
       </div>
     </>
   );
