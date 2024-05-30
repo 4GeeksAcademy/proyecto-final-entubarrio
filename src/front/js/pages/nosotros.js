5ddf978ccf8f6c8b553157e3fdd63316230e07fd
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2024-05-30 07:15:07 +0000

    vista nosotros texto negro

--- src/front/js/pages/nosotros.js
+++ src/front/js/pages/nosotros.js
@@ -13,7 +13,7 @@ export const Nosotros = () => {
 
             <h1>Bienvenido</h1><br></br>
 
-            <h3 className="text-danger-emphasis pb-3">¡En tu barrio te espera un mundo de posibilidades!</h3>
+            <h3 className=" pb-3">¡En tu barrio te espera un mundo de posibilidades!</h3>
             <div className=" text-center  pt-3 pb-3  ">
             </div>
 
@@ -22,7 +22,7 @@ export const Nosotros = () => {
             <div className="clearfix container">
                 <img src="01woman.jpg" className=" img-nosotros col-md-6 float-md-start mb-3 ms-md-4 " alt="..." />
 
-                <p className="text-danger-emphasis p-3">
+                <p className="p-3">
                     <strong>¿Cansado de las grandes superficies y de la impersonalidad de las compras online?</strong>
                 </p>
 
@@ -36,28 +36,28 @@ export const Nosotros = () => {
 
 
 
-            <h2 className="text-danger-emphasis p-3">¿Qué te ofrece En tu barrio?</h2>
+            <h2 className=" p-3">¿Qué te ofrece En tu barrio?</h2>
 
             <br></br>
 
             <div className="clearfix text-start">
                 <img src="02woman.jpg" className=" img-nosotros2 col-md-6 float-md-end mb-3 ms-md-4 " alt="..." />
 
-                <p className="text-danger-emphasis p-3">
+                <p className=" p-3">
                     <strong>Un centro comercial virtual para tu barrio: </strong>
                     Descubre una amplia variedad de productos y servicios a tu alcance,
                     desde tiendas de alimentación hasta librerías,
                     pasando por talleres de artesanía y mucho más. Todo ello, sin moverte de casa y con la comodidad de comprar online.
                 </p>
 
-                <p className="text-danger-emphasis m-3">
+                <p className=" m-3">
                     <strong>Apoya al comercio local: </strong>Cada compra que realizas En tu barrio contribuye a fortalecer la economía de tu barrio y a crear un futuro más sostenible.
                 </p>
-                <p className="text-danger-emphasis m-3">
+                <p className=" m-3">
                     <strong>Productos frescos y de calidad: </strong>Disfruta de productos frescos y de temporada de la mano de comercios locales que apuestan por la calidad y la atención al cliente.
                 </p>
 
-                <p className="text-danger-emphasis m-3">
+                <p className=" m-3">
                     <strong>Precios competitivos: </strong>Compara precios y encuentra las mejores ofertas en productos de tus tiendas favoritas.
                 </p>
 
